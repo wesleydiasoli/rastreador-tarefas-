5a3309da70293fc6911bb1e071a7db78b4444c89
wesleydias <wesleydias96@gmail.com>
2021-03-24 12:37:19 -0300

    Primeiro commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
+import Header from './components/Header'
+import Tasks from './components/Tasks'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const App = () => {
+    
+  const[tasks, setTasks] = useState([
+    {
+        id: 1,
+        text: 'Doctors Appointment',
+        day: 'Feb 5th at 2:30pm',
+        reminder: true,
+    },
+    {
+        id: 2,
+        text: 'Meeting at Home',
+        day: 'Feb 6th at 3:30pm',
+        reminder: true,
+    },
+    {
+        id: 1,
+        text: 'Make food',
+        day: 'Feb 5th at :30pm',
+        reminder: false,
+    },
+])
+
+//delete task
+const deleteTask = (id) => {
+  setTasks(tasks.filter((task) => task.id !== 
+  id))
 }
 
-export default App;
+  return (
+    <div className='container'>
+      <Header />
+      <Tasks tasks={tasks} onDele={deleteTask} />
+    </div>
+  )
+}
+
+
+export default App
